b7ff64a8d8b1158e54019c15d08087c07664d899
Emily Miskin <emilymiskin7@gmail.com>
2026-03-09 19:38:25 +0000

    Cleaned NAs from dataset

--- datasorting.R
+++ datasorting.R
@@ -5,14 +5,15 @@ library(dplyr)
 statsdata <- real.excel("statsdata.xlsx")
 
 wide_statsdata <- statsdata %>%
-  select('Country Name', `2016 [YR2016]`, `Series Name`) %>%  # keep only relevant columns
+  select(`Country Name`, `Series Name`, `2016 [YR2016]`) %>%
   pivot_wider(
-    names_from = 'Series Name',
-    values_from = '2016 [YR2016]'
-    )
+    names_from = `Series Name`, 
+    values_from = `2016 [YR2016]`,
+    values_fn = list(`2016 [YR2016]` = mean)
+  ) %>%
+  filter(!is.na(`GDP per capita (current US$)`) & !is.na(`Life expectancy at birth, total (years)`))
 
-#i cant ge the NAs to go away but if anyone can try that would be good
- 
+summary(wide_statsdata)
   
 
 
