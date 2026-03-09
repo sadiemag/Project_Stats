8b3d0d1ec63c23e9fcfda8a15422746d3e21864b
Emily Miskin <emilymiskin7@gmail.com>
2026-03-09 20:03:51 +0000

    part 1 and 2 done :)

--- datasorting.R
+++ datasorting.R
@@ -13,6 +13,16 @@ wide_statsdata <- statsdata %>%
   ) %>%
   filter(!is.na(`GDP per capita (current US$)`) & !is.na(`Life expectancy at birth, total (years)`))
   
+wide_statsdata %>%
+  summarise(
+    Mean = mean(`Life expectancy at birth, total (years)`),
+    Median = median(`Life expectancy at birth, total (years)`),
+    Std_Dev = sd(`Life expectancy at birth, total (years)`),
+    Min = min(`Life expectancy at birth, total (years)`),
+    Max = max(`Life expectancy at birth, total (years)`),
+    IQR = IQR(`Life expectancy at birth, total (years)`)
+  )
 
+wide_statsdata %>% filter(`Life expectancy at birth, total (years)` == min(`Life expectancy at birth, total (years)`))
 
-
+wide_statsdata %>% filter(`Life expectancy at birth, total (years)` == max(`Life expectancy at birth, total (years)`))
